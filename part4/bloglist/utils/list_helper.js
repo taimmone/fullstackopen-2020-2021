c95a06d6cd6005e0ce035b3f750700ea467012e7
Toni Immonen <toni.immonen@tuni.fi>
2021-01-06 09:34:00 +0200

    4.6: Find author with most blogs

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -5,8 +5,24 @@ const totalLikes = blogs => blogs.reduce((total, { likes }) => total + likes, 0)
 const favoriteBlog = blogs =>
   blogs.reduce((fav, blog) => (blog.likes > (fav?.likes ?? 0) ? blog : fav), null);
 
+const mostBlogs = blogs =>
+  blogs
+    ?.map(({ author }) => ({
+      author,
+      blogs: blogs.reduce(
+        (numberOfBlogs, blog) => (blog.author === author ? numberOfBlogs + 1 : numberOfBlogs),
+        0
+      ),
+    }))
+    .reduce(
+      (authorWithMostBlogs, authorBlogs) =>
+        authorBlogs.blogs > (authorWithMostBlogs?.blogs ?? 0) ? authorBlogs : authorWithMostBlogs,
+      null
+    );
+
 module.exports = {
   dummy,
   totalLikes,
   favoriteBlog,
+  mostBlogs,
 };
