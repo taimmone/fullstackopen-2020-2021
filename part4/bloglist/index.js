bfa19a688e0d9e42b2a4de29440837d8247017cd
Toni Immonen <toni.immonen@tuni.fi>
2021-01-06 09:47:42 +0200

    Listen with 'server' instead of 'app'

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -5,7 +5,7 @@ const logger = require('./utils/logger');
 
 const server = http.createServer(app);
 
-app.listen(config.PORT, () => {
+server.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`);
 });
 
