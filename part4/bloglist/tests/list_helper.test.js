530d7fd6637587ec3950939ef7e34ff34be4f596
Toni Immonen <toni.immonen@tuni.fi>
2021-01-06 09:06:16 +0200

    4.5: Find blog with most likes

--- part4/bloglist/tests/list_helper.test.js
+++ part4/bloglist/tests/list_helper.test.js
@@ -7,38 +7,38 @@ test('dummy returns one', () => {
   expect(result).toBe(1);
 });
 
+const listWithOneBlog = [
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    __v: 0,
+  },
+];
+
+const listWithThreeBlogs = [
+  listWithOneBlog[0],
+  {
+    _id: '5ff0021bca2ed0368aaa2a7b',
+    title: 'A test blog',
+    author: 'Abe Cee',
+    url: 'about:blank',
+    likes: 3,
+    __v: 0,
+  },
+  {
+    _id: '5ff00305ca2ed0368aaa2a7c',
+    title: 'Another blog',
+    author: 'Dee Fegh',
+    url: 'about:blank',
+    likes: 7,
+    __v: 0,
+  },
+];
+
 describe('total likes', () => {
-  const listWithOneBlog = [
-    {
-      _id: '5a422aa71b54a676234d17f8',
-      title: 'Go To Statement Considered Harmful',
-      author: 'Edsger W. Dijkstra',
-      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-      likes: 5,
-      __v: 0,
-    },
-  ];
-
-  const listWithThreeBlogs = [
-    listWithOneBlog[0],
-    {
-      _id: '5ff0021bca2ed0368aaa2a7b',
-      title: 'A test blog',
-      author: 'Abe Cee',
-      url: 'about:blank',
-      likes: 3,
-      __v: 0,
-    },
-    {
-      _id: '5ff00305ca2ed0368aaa2a7c',
-      title: 'Another blog',
-      author: 'Dee Fegh',
-      url: 'about:blank',
-      likes: 7,
-      __v: 0,
-    },
-  ];
-
   test('of empty list is zero', () => {
     const result = listHelper.totalLikes([]);
     expect(result).toBe(0);
@@ -52,5 +52,22 @@ describe('total likes', () => {
   test('of a bigger list is calculated right', () => {
     const result = listHelper.totalLikes(listWithThreeBlogs);
     expect(result).toBe(15);
+  });
+});
+
+describe('favorite blog', () => {
+  test('of empty list is null', () => {
+    const result = listHelper.favoriteBlog([]);
+    expect(result).toBe(null);
+  });
+
+  test('when list has only one blog equals the blog', () => {
+    const result = listHelper.favoriteBlog(listWithOneBlog);
+    expect(result).toEqual(listWithOneBlog[0]);
+  });
+
+  test('of a bigger list is correct', () => {
+    const result = listHelper.favoriteBlog(listWithThreeBlogs);
+    expect(result).toEqual(listWithThreeBlogs[2]);
   });
 });
--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,7 +2,11 @@ const dummy = () => 1;
 
 const totalLikes = blogs => blogs.reduce((total, { likes }) => total + likes, 0);
 
+const favoriteBlog = blogs =>
+  blogs.reduce((fav, blog) => (blog.likes > (fav?.likes ?? 0) ? blog : fav), null);
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
