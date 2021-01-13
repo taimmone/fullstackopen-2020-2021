3172a93fda646ef14007675d947f683fcd3973e9
Toni Immonen <toni.immonen@tuni.fi>
2021-01-13 02:16:19 +0200

    4.10: Blog list tests, step3

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -15,22 +15,48 @@ beforeEach(async () => {
   }
 });
 
-test('blogs are returned as json', async () =>
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/));
+describe('GET /api/blogs', () => {
+  test('blogs are returned as json', async () =>
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/));
 
-test('all blogs are returned', async () => {
-  const res = await api.get('/api/blogs');
-  expect(res.body).toHaveLength(helper.initialBlogs.length);
+  test('all blogs are returned', async () => {
+    const res = await api.get('/api/blogs');
+    expect(res.body).toHaveLength(helper.initialBlogs.length);
+  });
+
+  test('unique identifier property is named "id", not "_id"', async () => {
+    const res = await api.get('/api/blogs');
+    const blog = res.body[0];
+    expect(blog.id).toBeDefined();
+    expect(blog._id).toBe(undefined);
+  });
 });
 
-test('unique identifier property is named "id", not "_id"', async () => {
-  const res = await api.get('/api/blogs');
-  const blog = res.body[0];
-  expect(blog.id).toBeDefined();
-  expect(blog._id).toBe(undefined);
+describe('POST /api/blogs', () => {
+  test('creates a new blog post', async () => {
+    const newBlog = {
+      title: 'Towards an automated changelog workflow',
+      author: 'William Woodruff',
+      url: 'https://blog.yossarian.net/2020/11/05/Towards-an-automated-changelog-workflow',
+      likes: 1,
+    };
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/);
+
+    const res = await api.get('/api/blogs');
+
+    expect(res.body).toHaveLength(helper.initialBlogs.length + 1);
+    expect(
+      res.body.map(({ title, author, url, likes }) => ({ title, author, url, likes }))
+    ).toContainEqual(newBlog);
+  });
 });
 
 afterAll(() => mongoose.connection.close());
